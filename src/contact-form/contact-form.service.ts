f52b0f21c85b71e7e06a6e904d888eb92102e252
DevanandGit <nandhudevanand4419@gmail.com>
2025-10-25 19:43:09 +0530

    bugs cleared

--- src/contact-form/contact-form.service.ts
+++ src/contact-form/contact-form.service.ts
@@ -15,26 +15,9 @@ export class ContactFormService {
     async submitForm(dto: CreateContactFormDto) {
         const { Name, phone_number, email, message } = dto;
 
-        // Save form to DB
-        // const form = await this.prisma.jobApplicationform.create({
-        //     data: {
-        //         first_name: first_name,
-        //         last_name: last_name,
-        //         phone_number: phone_number,
-        //         drive_link: drive_link,
-        //         comments: comments,
-        //         job_opening_id,
-        //     },
-        // });
-
-        // Fetch job title (fixing your bug: `id` was undefined)
-        // const job = await this.prisma.jobOpenings.findUnique({
-        //     where: { id: job_opening_id },
-        // });
-
         // Send email to admin
         await this.mailService.sendMail({
-            to: email,
+            to: process.env.MAIL_USER,
             subject: `New Enquiry from ${Name}`,
             template: 'welcome', // looks for src/templates/welcome.pug
             context: {
